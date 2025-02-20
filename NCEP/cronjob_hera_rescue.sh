13282ff89b4a3298f935e16bdf0161582f4036bb
LinlinCui-NOAA <Linlin.Cui@noaa.gov>
2025-02-20 20:21:57 +0000

    more updates on job description

--- NCEP/cronjob_hera_rescue.sh
+++ NCEP/cronjob_hera_rescue.sh
@@ -1,11 +1,11 @@
 #!/bin/bash --login
 
-echo "Job 1 is running"
+echo "Job 1 prepdata is running"
 sh /scratch1/NCEPDEV/nems/Linlin.Cui/Tests/FourCastNetv2/realtime_forecast/fcn_prepdata_hera.sh $1
 sleep 60  # Simulating some work
-echo "Job 1 completed"
+echo "Job 1 prepdata completed"
 
-echo "Job 2 is running"
+echo "Job 2 runfcst is running"
 job2_id=$(sbatch /scratch1/NCEPDEV/nems/Linlin.Cui/Tests/FourCastNetv2/realtime_forecast/fcn_runfcst_hera_cpu.sh $1 | awk '{print $4}')
 
 # Wait for job 2 to complete
@@ -13,9 +13,9 @@ while squeue -j $job2_id &>/dev/null; do
     sleep 5  # Adjust the polling interval as needed
 done
 sleep 5  # Simulating some work
-echo "Job 2 completed"
+echo "Job 2 runfcst completed"
 
-echo "Job 3 is running"
+echo "Job 3 upload data to s3 is running"
 sh /scratch1/NCEPDEV/nems/Linlin.Cui/Tests/FourCastNetv2/realtime_forecast/fcn_datadissm_hera.sh $1
 sleep 5
-echo "Job 3 completed"
+echo "Job 3 upload data to s3 completed"
